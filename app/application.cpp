273f95a751fee3a35e7be2d7717f84123e467f42
Joakim Plate <elupus@ecce.se>
2016-11-15 21:48:01 +0100

    Move out all the helper defines to separate header file

--- app/application.cpp
+++ app/application.cpp
@@ -18,6 +18,7 @@
 #include <user_config.h>
 #include <SmingCore.h>
 #include <libraries/DS18S20/ds18s20.h>
+#include "knx.h"
 
 void data_received(UdpConnection& connection, char *data, int size, IPAddress remoteIP, uint16_t remotePort);
 
@@ -47,89 +48,13 @@ void networkOk()
     }
 }
 
-
-#define KNX_SERVICEFAMILIY_2                 0x2u
-#define KNX_SERVICEFAMILIY_ROUTING           0x5u
-
-#define KNX_SERVICE_SEARCH_REQ               0x201
-#define KNX_SERVICE_SEARCH_RES               0x202
-#define KNX_SERVICE_DESCRIPTION_REQ          0x203
-#define KNX_SERVICE_DESCRIPTION_RES          0x204
-#define KNX_SERVICE_CONNECT_REQ              0x205
-#define KNX_SERVICE_CONNECT_RES              0x206
-#define KNX_SERVICE_CONNECTIONSTATE_REQ      0x207
-#define KNX_SERVICE_CONNECTIONSTATE_RES      0x208
-#define KNX_SERVICE_DISCONNECT_REQ  521      0x209
-#define KNX_SERVICE_DISCONNECT_RES  522      0x20A
-#define KNX_SERVICE_DEVICE_CONFIGURATION_REQ 0x310
-#define KNX_SERVICE_DEVICE_CONFIGURATION_ACK 0x311
-#define KNX_SERVICE_TUNNELING_REQ            0x420
-#define KNX_SERVICE_TUNNELING_ACK            0x421
-#define KNX_SERVICE_ROUTING_IND              0x530
-#define KNX_SERVICE_ROUTING_LOST_MSG         0x531
-
-#define KNX_PRIORITY_SYSTEM         0u
-#define KNX_PRIORITY_HIGH           1u
-#define KNX_PRIORITY_ALARM          2u
-#define KNX_PRIORITY_NORMAL         3u
-
-#define KNX_APCI_VALUE_READ                    0b0000000000u
-#define KNX_APCI_VALUE_RESPONSE                0b0001000000u
-#define KNX_APCI_VALUE_WRITE                   0b0010000000u
-#define KNX_APCI_MEMORY_WRITE                  0b1010000000u
-
-#define KNX_HOST_PROTOCOL_IPV4_UDP  1u
-#define KNX_HOST_PROTOCOL_IPV4_TCP  2u
-
-#define KNX_DESCRIPTION_TYPE_DEVICE_INFO       0x01u
-#define KNX_DESCRIPTION_TYPE_SUPP_SVC_FAMILIES 0x02u
-#define KNX_DESCRIPTION_TYPE_IP_CONFIG         0x03u
-#define KNX_DESCRIPTION_TYPE_IP_CUR_CONFIG     0x04u
-#define KNX_DESCRIPTION_TYPE_KNX_ADDRESSES     0x05u
-#define KNX_DESCRIPTION_TYPE_MFR_DATA          0xFEu
-
-#define KNX_DEVICE_MEDIUM_TP1                  0x02u
-#define KNX_DEVICE_MEDIUM_PL110                0x04u
-#define KNX_DEVICE_MEDIUM_RF                   0x10u
-#define KNX_DEVICE_MEDIUM_KNX_IP               0x20u
-
 #define KNX_FIELD_HEADER_LEN 0x06u
 #define KNX_FIELD_HEADER_VER 0x10u
 #define KNX_ROUTING_COUNTER 6u /* fixed counter, since data originate on this device */
 
-#define KNX_FRAME_FORMAT_EXT    0u
-#define KNX_FRAME_FORMAT_STD    1u
-
-#define KNX_EXTFRAME_FORMAT_STD 0u
-
-#define KNX_MESSAGE_CODE_LDATA_CON 0x2Eu
-#define KNX_MESSAGE_CODE_LDATA_IND 0x29u
-#define KNX_MESSAGE_CODE_LDATA_REQ 0x11u
-
-#define KNX_FIELD_CONTROL1_FORMAT_OFFSET 7u
-#define KNX_FIELD_CONTROL1_REPEAT_OFFSET 5u
-#define KNX_FIELD_CONTROL1_BROADCAST_OFFSET 4u
-#define KNX_FIELD_CONTROL1_PRIO_OFFSET   2u
-#define KNX_FIELD_CONTROL1_ACKREQ_OFFSET 1u
-#define KNX_FIELD_CONTROL1_CONFIRM_OFFSET 0u
-
-#define KNX_FIELD_CONTROL1_RESERVED      0b00100000
-
-#define KNX_FIELD_CONTROL2_DESTTYPE_OFFSET      7u
-#define KNX_FIELD_CONTROL2_HOPCOUNT_OFFSET      4u
-#define KNX_FIELD_CONTROL2_EXTFRAME_OFFSET      0u
-
-#define KNX_FIELD_COMMAND_COMMAND_OFFSET 6u
-#define KNX_FIELD_COMMAND_DATA_OFFSET    0u
-
-#define KNX_GROUP_ADDRESS(a, b, c) ((a) << 11u | (b) << 8 | (c))
-#define KNX_DEVICE_ADDRESS(area, line, device) ((area) << 12u | (line) << 8u | (device))
-
-
 #define KNX_CONFIG_DEVICE_ADDRESS          KNX_DEVICE_ADDRESS(1, 2, 0)
 #define KNX_CONFIG_DEVICE_HARDWARE         0u
 #define KNX_CONFIG_DEVICE_SERVICE_FAMILIES 0u
-
 
 void set_u16(char buf[2], uint16_t val)
 {
---
+++ include/knx.h
@@ -0,0 +1,92 @@
+/* Copyright (C) 2016 Joakim Plate
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef INCLUDE_KNX_H_
+#define INCLUDE_KNX_H_
+
+
+#define KNX_SERVICEFAMILIY_2                   0x2u
+#define KNX_SERVICEFAMILIY_ROUTING             0x5u
+
+#define KNX_SERVICE_SEARCH_REQ                 0x201
+#define KNX_SERVICE_SEARCH_RES                 0x202
+#define KNX_SERVICE_DESCRIPTION_REQ            0x203
+#define KNX_SERVICE_DESCRIPTION_RES            0x204
+#define KNX_SERVICE_CONNECT_REQ                0x205
+#define KNX_SERVICE_CONNECT_RES                0x206
+#define KNX_SERVICE_CONNECTIONSTATE_REQ        0x207
+#define KNX_SERVICE_CONNECTIONSTATE_RES        0x208
+#define KNX_SERVICE_DISCONNECT_REQ  521        0x209
+#define KNX_SERVICE_DISCONNECT_RES  522        0x20A
+#define KNX_SERVICE_DEVICE_CONFIGURATION_REQ   0x310
+#define KNX_SERVICE_DEVICE_CONFIGURATION_ACK   0x311
+#define KNX_SERVICE_TUNNELING_REQ              0x420
+#define KNX_SERVICE_TUNNELING_ACK              0x421
+#define KNX_SERVICE_ROUTING_IND                0x530
+#define KNX_SERVICE_ROUTING_LOST_MSG           0x531
+
+#define KNX_PRIORITY_SYSTEM                    0u
+#define KNX_PRIORITY_HIGH                      1u
+#define KNX_PRIORITY_ALARM                     2u
+#define KNX_PRIORITY_NORMAL                    3u
+
+#define KNX_APCI_VALUE_READ                    0b0000000000u
+#define KNX_APCI_VALUE_RESPONSE                0b0001000000u
+#define KNX_APCI_VALUE_WRITE                   0b0010000000u
+#define KNX_APCI_MEMORY_WRITE                  0b1010000000u
+
+#define KNX_HOST_PROTOCOL_IPV4_UDP             1u
+#define KNX_HOST_PROTOCOL_IPV4_TCP             2u
+
+#define KNX_DESCRIPTION_TYPE_DEVICE_INFO       0x01u
+#define KNX_DESCRIPTION_TYPE_SUPP_SVC_FAMILIES 0x02u
+#define KNX_DESCRIPTION_TYPE_IP_CONFIG         0x03u
+#define KNX_DESCRIPTION_TYPE_IP_CUR_CONFIG     0x04u
+#define KNX_DESCRIPTION_TYPE_KNX_ADDRESSES     0x05u
+#define KNX_DESCRIPTION_TYPE_MFR_DATA          0xFEu
+
+#define KNX_DEVICE_MEDIUM_TP1                  0x02u
+#define KNX_DEVICE_MEDIUM_PL110                0x04u
+#define KNX_DEVICE_MEDIUM_RF                   0x10u
+#define KNX_DEVICE_MEDIUM_KNX_IP               0x20u
+
+#define KNX_FRAME_FORMAT_EXT                   0u
+#define KNX_FRAME_FORMAT_STD                   1u
+
+#define KNX_EXTFRAME_FORMAT_STD                0u
+
+#define KNX_MESSAGE_CODE_LDATA_CON             0x2Eu
+#define KNX_MESSAGE_CODE_LDATA_IND             0x29u
+#define KNX_MESSAGE_CODE_LDATA_REQ             0x11u
+
+#define KNX_FIELD_CONTROL1_FORMAT_OFFSET       7u
+#define KNX_FIELD_CONTROL1_REPEAT_OFFSET       5u
+#define KNX_FIELD_CONTROL1_BROADCAST_OFFSET    4u
+#define KNX_FIELD_CONTROL1_PRIO_OFFSET         2u
+#define KNX_FIELD_CONTROL1_ACKREQ_OFFSET       1u
+#define KNX_FIELD_CONTROL1_CONFIRM_OFFSET      0u
+
+#define KNX_FIELD_CONTROL2_DESTTYPE_OFFSET     7u
+#define KNX_FIELD_CONTROL2_HOPCOUNT_OFFSET     4u
+#define KNX_FIELD_CONTROL2_EXTFRAME_OFFSET     0u
+
+#define KNX_GROUP_ADDRESS(a, b, c) ((a) << 11u | (b) << 8 | (c))
+#define KNX_DEVICE_ADDRESS(area, line, device) ((area) << 12u | (line) << 8u | (device))
+
+void knx_init(void);
+
+#endif /* INCLUDE_KNX_H_ */
